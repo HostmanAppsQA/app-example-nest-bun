49e12f8c185fbe9cc7969c374cd06fafa6e83daa
Коряковский Георгий <g.koryakovskiy@timeweb.ru>
2026-05-28 14:40:33 +0300

    feat: initialization

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000, '0.0.0.0');
+    console.log(`Server running on ${await app.getUrl()}`);
+}
+
+bootstrap();
